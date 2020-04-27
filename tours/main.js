6ff2a31bd0cc607735361ea44a5522b7b696a8a4
mehediemon007 <34378422+mehediemon007@users.noreply.github.com>
2020-04-27 22:28:07 +0600

    initial commit

---
+++ tours/main.js
@@ -0,0 +1,12 @@
+//function expression to select element
+const selectElement = (s) => document.querySelector(s);
+//Open menu on click
+selectElement('.open').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.add('active');
+});
+
+//Close menu on click
+
+selectElement('.close').addEventListener('click', () =>{
+    selectElement('.nav-list').classList.remove('active');
+});
